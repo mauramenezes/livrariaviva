6759dbce711d3c2d9ad071fdc64d9e92bbd06705
Maura Menezes <mauramenezespessoal@gmail.com>
2023-12-15 13:51:09 -0300

    Primeiro commit*

---
+++ src/main/java/br/com/livraria/livrariaviva/LivrariavivaApplication.java
@@ -0,0 +1,13 @@
+package br.com.livraria.livrariaviva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrariavivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrariavivaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/livraria/livrariaviva/LivrariavivaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.livraria.livrariaviva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrariavivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
