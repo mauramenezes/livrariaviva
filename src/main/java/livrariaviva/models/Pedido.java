8a95de564b72a4133612a96c3232d3e9d8e76919
Maura Menezes <mauramenezespessoal@gmail.com>
2023-12-29 14:14:57 -0300

    classe pedido

--- src/main/java/livrariaviva/models/Pedido.java
+++ src/main/java/livrariaviva/models/Pedido.java
@@ -15,5 +15,55 @@ public class Pedido {
     private int idpedido;
     private DateTimeLiteralExpression.DateTime datapedido;
     private float total;
+    private int idcliente, idlivro;
+
+    public Pedido(DateTimeLiteralExpression.DateTime datapedido, float total, int idcliente, int idlivro) {
+        this.datapedido = datapedido;
+        this.total = total;
+        this.idcliente = idcliente;
+        this.idlivro = idlivro;
+    }
+
+    public int getIdpedido() {
+        return idpedido;
+    }
+
+    public DateTimeLiteralExpression.DateTime getDatapedido() {
+        return datapedido;
+    }
+
+    public void setDatapedido(DateTimeLiteralExpression.DateTime datapedido) {
+        this.datapedido = datapedido;
+    }
+
+    public float getTotal() {
+        return total;
+    }
+
+    public void setTotal(float total) {
+        this.total = total;
+    }
+
+    public int getIdcliente() {
+        return idcliente;
+    }
+
+    public void setIdcliente(int idcliente) {
+        this.idcliente = idcliente;
+    }
+
+    public int getIdlivro() {
+        return idlivro;
+    }
+
+    public void setIdlivro(int idlivro) {
+        this.idlivro = idlivro;
+    }
+
+    
+  
+  
+
+    
     
 }
